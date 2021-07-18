ebee24b2f80cc7c8ca88dafc36d94286bb36690c
cmccray87 <cody.mccray87@gmail.com>
2021-07-18 15:45:15 -0500

    added taskhandler and refresh prevent

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function() {
+
+    event.preventDefault();
+    console.log(event);
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "this is a new task.";
@@ -10,4 +14,4 @@ var createTaskHandler = function() {
 };
 
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
